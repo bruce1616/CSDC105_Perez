e52ec0d5c8af8c64b4c3a26380bf091404c2bb16
bruce1616 <jerperez@gbox.adnu.edu.ph>
2020-02-27 20:59:25 +0800

    Remove le13

--- School/app/views/subjects/_subject.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! subject, :id, :Subject, :Title, :Credit, :Units, :Descriptions, :created_at, :updated_at
-json.url subject_url(subject, format: :json)
--- School/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  resources :subjects
-  resources :teachers
-  resources :students
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
